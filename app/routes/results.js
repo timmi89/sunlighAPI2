24c05eca656e68d3a6ba49a5dd0178f1e9b28aa7
Hillary Hintzen, Lindsey Raffa and Timmi McGuire <hillary@>
2016-04-05 13:44:35 -0700

    create env file and variable  to hide our api key

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=697fcfc7dbdc42299078fcdf71835f15&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
